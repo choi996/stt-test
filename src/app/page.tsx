a4e83c3c17cceafcf0f1c134f0fdeb53729738d3
hhchoi <choi902307@gmail.com>
2024-10-15 10:10:26 +0900

    fix: 체크박스 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,13 @@ import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [engineoilChecked, setEngineoilChecked] = useState(false);
+  const [batterylChecked, setBatteryChecked] = useState(false);
+  const [tireChecked, setTireChecked] = useState(false);
+
   const {
     transcript,
     listening,
@@ -13,9 +18,33 @@ export default function Home() {
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
+  useEffect(() => {
+    if (transcript) {
+      if (transcript.includes("엔진오일")) {
+        setEngineoilChecked(true);
+        resetTranscript();
+      }
+      if (transcript.includes("배터리")) {
+        setBatteryChecked(true);
+        resetTranscript();
+      }
+      if (transcript.includes("타이어")) {
+        setTireChecked(true);
+        resetTranscript();
+      }
+    }
+  }, [transcript, resetTranscript]);
+
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser does support speech recognition.</span>;
   }
+
+  const reset = () => {
+    resetTranscript();
+    setEngineoilChecked(false);
+    setBatteryChecked(false);
+    setTireChecked(false);
+  };
 
   return (
     <div className={styles.page}>
@@ -25,7 +54,6 @@ export default function Home() {
           <button
             onClick={() =>
               SpeechRecognition.startListening({
-                language: "ko-KR",
                 continuous: true,
               })
             }
@@ -35,9 +63,42 @@ export default function Home() {
           <button onClick={() => SpeechRecognition.stopListening()}>
             Stop
           </button>
-          <button onClick={resetTranscript}>Reset</button>
+          <button onClick={reset}>Reset</button>
         </div>
-        <p style={{ marginTop: 40 }}>{transcript}</p>
+        {/* <p style={{ marginTop: 40 }}>{transcript}</p> */}
+
+        <div className={styles.check_wrapper}>
+          <div>
+            <input
+              type="checkbox"
+              id="engineoil"
+              name="engineoil"
+              checked={engineoilChecked}
+              onClick={() => setEngineoilChecked((prev) => !prev)}
+            />
+            <label htmlFor="engineoil">엔진오일</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="battery"
+              name="battery"
+              checked={batterylChecked}
+              onClick={() => setBatteryChecked((prev) => !prev)}
+            />
+            <label htmlFor="battery">배터리</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="tire"
+              name="tire"
+              checked={tireChecked}
+              onClick={() => setTireChecked((prev) => !prev)}
+            />
+            <label htmlFor="battery">타이어</label>
+          </div>
+        </div>
       </div>
     </div>
   );
