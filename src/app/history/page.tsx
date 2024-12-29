e7e589223141ad44b9db1c1a49f489d4f3c281dd
hhchoi <choi902307@gmail.com>
2024-12-30 08:19:59 +0900

    fix

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -8,7 +8,7 @@ const dummy = [
   {
     createdAt: '2024.12.24',
     repairShopName: '왕십리 역사',
-    managerName: '호날두',
+    managerName: '홍길동',
     list: [
       '엔진오일 교환',
       '앞 와이퍼 플레이드 교환',
@@ -19,7 +19,7 @@ const dummy = [
   {
     createdAt: '2024.12.23',
     repairShopName: '서울 SKOK',
-    managerName: '메시',
+    managerName: '김스메',
     list: [
       '에어컨 컴프레셔 교환',
       '에어컨계통 작업 후 에어컨 가스 완충',
@@ -30,7 +30,7 @@ const dummy = [
   {
     createdAt: '2024.12.22',
     repairShopName: '패밀리마트 종로2가점',
-    managerName: '음바페',
+    managerName: '홍길동',
     list: ['타이어 2개 교환', '앞 브레이크 패드 교환', '배터리 교체'],
     distance: 30,
   },
@@ -38,7 +38,7 @@ const dummy = [
 
 export default function HistoryPage() {
   return (
-    <div className="h-dvh">
+    <div className="h-full">
       <Header title="정비내역" />
       <div className="py-10 px-20 flex items-center justify-between border-b-10 border-gray10">
         <div className="flex items-center gap-4">
